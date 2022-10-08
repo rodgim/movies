c8abba4c2ffb79e1326ec78926734a14c3502927
Rodrigo Gimenez <rodrigo_g90@hotmail.es>
2022-10-08 11:45:16 -0300

    Add local database

---
+++ app/src/main/java/com/rodgim/movies/framework/database/MovieDao.kt
@@ -0,0 +1,26 @@
+package com.rodgim.movies.framework.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface MovieDao {
+
+    @Query("SELECT * FROM Movie")
+    fun getAll(): List<Movie>
+
+    @Query("SELECT * FROM Movie WHERE id = :id")
+    fun findById(id: Int): Movie
+
+    @Query("SELECT COUNT(id) FROM Movie")
+    fun movieCount(): Int
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertMovies(movies: List<Movie>)
+
+    @Update
+    fun updateMovie(movie: Movie)
+}
